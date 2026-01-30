a3d2bc5bb471553011c44c9d5a9e512a9f975775
boaz65sa-byte <boaz65sa@gmail.com>
2026-01-30 12:06:59 +0200

    Update musicData.js

--- src/data/musicData.js
+++ src/data/musicData.js
@@ -1,4 +1,23 @@
-// נתוני מודוסים יווניים (דרומוס)
+// src/data/musicData.js
+
+// תדרים בסיסיים לחישוב (אוקטבה 4)
+const BASE_FREQ = {
+  'C': 261.63, 'C#': 277.18, 'D': 293.66, 'Eb': 311.13, 'E': 329.63, 'F': 349.23,
+  'F#': 369.99, 'G': 392.00, 'G#': 415.30, 'A': 440.00, 'Bb': 466.16, 'B': 493.88
+};
+
+// כיוון בוזוקי סטנדרטי (CFAD) - מלמטה למעלה (מיתר דק למיתר עבה)
+// D4 (רה), A3 (לה), F3 (פה), C3 (דו)
+export const STRING_TUNING = [
+  { name: 'D', octave: 4, baseFreq: 293.66 },
+  { name: 'A', octave: 3, baseFreq: 220.00 },
+  { name: 'F', octave: 3, baseFreq: 174.61 },
+  { name: 'C', octave: 3, baseFreq: 130.81 }
+];
+
+export const NOTES = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B'];
+export const FRET_COUNT = 15;
+
 export const DROMOI = {
   'Rast (Major)': [0, 2, 4, 5, 7, 9, 11],
   'Hitzaz': [0, 1, 4, 5, 7, 8, 10],
@@ -7,55 +26,25 @@ export const DROMOI = {
   'Niavent': [0, 2, 3, 6, 7, 8, 11],
   'Sambah': [0, 2, 3, 4, 7, 8, 10],
   'Kiourdi': [0, 2, 3, 5, 7, 9, 10]
-}
+};
 
-// נתוני אקורדים (מספר הסריג על כל מיתר)
+// עדכון האקורדים שיתאימו למבנה החדש
 export const CHORDS = {
-  'D Major': [0, 0, 2, 2],
-  'D Minor': [0, 0, 1, 2],
-  'D7': [0, 3, 2, 2],
-  'G Major': [5, 5, 4, 7],
-  'A Major': [7, 7, 6, 9],
-  'F Major': [3, 3, 2, 5],
-  'C Major': [10, 10, 9, 12],
-  'E Minor': [2, 2, 2, 4]
-}
+  'D Major': [2, 0, 0, 2], // מיתר 1 (D) עד 4 (C)
+  'D Minor': [1, 0, 0, 2],
+  'C Major': [0, 3, 5, 5], 
+  'F Major': [1, 0, 2, 3],
+  'G Major': [3, 2, 0, 0]
+};
 
-// שמות התווים
-export const NOTES = ['C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B']
+// פונקציית עזר לחישוב תדר של תו ספציפי
+export const getNoteFreq = (baseFreq, fret) => {
+  return baseFreq * Math.pow(2, fret / 12);
+};
 
-// כיוון מיתרים (מלמעלה למטה)
-export const STRINGS = ['D', 'A', 'F', 'C']
-export const OPEN_NOTES = [2, 9, 5, 0] // מיקום בתוך מערך NOTES
-
-// מספר סריגים
-export const FRET_COUNT = 15
-
-// פונקציה לחישוב שם תו לפי מיתר וסריג
 export const getNoteName = (stringIndex, fret) => {
-  const noteIndex = (OPEN_NOTES[stringIndex] + fret) % 12
-  return NOTES[noteIndex]
-}
-
-// תיאורי מודוסים
-export const MODE_DESCRIPTIONS = {
-  'Rast (Major)': 'המקם הבסיסי ביותר, מקביל למז\'ור המערבי. משמש בשירים שמחים וחגיגיים.',
-  'Hitzaz': 'מקם עם צליל מזרחי אופייני, משתמש ב-2 minor ו-5 diminished. נפוץ במוזיקה טורקית.',
-  'Ousak': 'מקם מינורי פריגי עם אופי מלנכולי. נפוץ מאוד במוזיקה יוונית מסורתית.',
-  'Pireotikos': 'מקם דרמטי עם שישית מוגבהת, נותן תחושה של מתח ורגש עמוק.',
-  'Niavent': 'מקם מינורי הרמוני עם אופי טורקי-ערבי. משמש ביצירות מלודרמטיות.',
-  'Sambah': 'מקם נדיר עם שניות קטנות, יוצר צבע הרמוני ייחודי.',
-  'Kiourdi': 'גרסה מינורית של Rast, עם אופי עצוב אך לא דרמטי.'
-}
-
-// תיאורי אקורדים
-export const CHORD_DESCRIPTIONS = {
-  'D Major': 'אקורד מז\'ורי בסיסי בפוזיציה פתוחה',
-  'D Minor': 'אקורד מינורי בסיסי, מלנכולי',
-  'D7': 'אקורד דומיננט, משמש למעברים',
-  'G Major': 'אקורד G בפוזיציה נמוכה',
-  'A Major': 'אקורד A בפוזיציה בינונית',
-  'F Major': 'אקורד F בפוזיציה נמוכה',
-  'C Major': 'אקורד C בפוזיציה גבוהה',
-  'E Minor': 'אקורד מינורי בסיסי'
-}
+  // המיתר הראשון במערך STRING_TUNING הוא D (אינדקס 2 ברשימת התווים)
+  const startNote = STRING_TUNING[stringIndex].name; 
+  const startIndex = NOTES.indexOf(startNote);
+  return NOTES[(startIndex + fret) % 12];
+};
